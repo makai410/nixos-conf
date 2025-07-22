9ee251cbb5dcabd5c48725d5ba9f9471634a52fe
Makai <m4kai410@gmail.com>
2025-07-23 02:57:23 +0800

    fastfetch nixos logo

--- hosts/home-manager-common-config.nix
+++ hosts/home-manager-common-config.nix
@@ -71,9 +71,9 @@
 {
     "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
     "logo": {
-        "source": "~/.local/share/fastfetch/ascii/cat.txt",
+        "source": "nixos_small",
         "padding": {
-            "top": 2,
+            "top": 6,
             "right": 6
         }
     },
