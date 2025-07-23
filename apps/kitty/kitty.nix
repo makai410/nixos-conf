9c05f882a4722fa9fcdb761d1d6ad7be63a96183
Makai <m4kai410@gmail.com>
2025-07-23 20:37:57 +0800

    change kitty font size

--- apps/kitty/kitty.nix
+++ apps/kitty/kitty.nix
@@ -7,7 +7,7 @@
       scrollback_lines = 10000;
       enable_audio_bell = false;
       update_check_interval = 0;
-      font_size = "16";
+      font_size = "12";
       window_padding_width = "10 10";
       cursor_shape = "block";
       shell_integration = "no-cursor";
